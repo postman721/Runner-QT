4b440da71a270b93c2df2c90a991b0d37e9b7def
JJ Posti <jjposti@techtimejourney.net>
2017-08-17 21:11:05 +0300

    upgraded to 3.0.1
    Obsolete lines removed. 49 of code is the current total.

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-#Runner-QT v.3 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
+#Runner-QT v.3.0.1 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
 #!/usr/bin/env python
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt  
 from PyQt5.QtWidgets import *
 import subprocess, os, sys
 from subprocess import Popen
-
 class Main(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Main, self).__init__(*args, **kwargs)
@@ -14,7 +13,6 @@ class Main(QMainWindow):
         self.setStyleSheet("color:#ffffff; background-color:#353535; border: 2px solid #353535; border-radius: 3px;font-size: 12px;")
         self.move(QApplication.desktop().screen().rect().center()- self.rect().center())
         self.setFixedSize(540, 43)
-        self.vertical = QVBoxLayout()
 #Text field
         self.address=QLineEdit()
         self.address.setStyleSheet("QLineEdit{color:#ffffff; background-color:#2a2a2a; border: 2px solid #353535; border-radius: 3px;font-size: 12px;}"
@@ -35,11 +33,9 @@ class Main(QMainWindow):
         self.toolbar.addWidget(self.address)
         self.toolbar.addWidget(self.about1)
         self.addToolBar(self.toolbar)
-        self.toolbar.setLayout(self.vertical)  
-        self.setLayout(self.vertical)
 #About messagebox
     def about(self):
-        buttonReply = QMessageBox.information(self, 'Runner-QT v.3 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix", QMessageBox.Ok )
+        buttonReply = QMessageBox.information(self, 'Runner-QT v.3.0.1 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix", QMessageBox.Ok )
         if buttonReply == QMessageBox.Ok:
             pass
     def navigatex(self):
