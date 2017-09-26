87ca5e84bb537bf7debda4c8f38471aecfd93435
JJ Posti <jjposti@techtimejourney.net>
2017-09-26 15:47:11 +0300

    v.4 upgrade
    Changes in v.4:
    When Runner-QT exits(from v.4 onwards) it prints an error to the cli if a program it tried to execute did not exist. Runner-QT v.4 quits with an ESC key - automatic exit functionality remains as a feature.

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
-#Runner-QT v.3.0.1 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
+#Runner-QT v.4 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
 #!/usr/bin/env python
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt  
+from PyQt5.QtCore import *  
 from PyQt5.QtWidgets import *
 import subprocess, os, sys
 from subprocess import Popen
 class Main(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Main, self).__init__(*args, **kwargs)
-#Title & Layout		
+#Title & Layout	& Label	
         self.setWindowTitle("Runner-QT")
         self.setStyleSheet("color:#ffffff; background-color:#353535; border: 2px solid #353535; border-radius: 3px;font-size: 12px;")
         self.move(QApplication.desktop().screen().rect().center()- self.rect().center())
-        self.setFixedSize(540, 43)
+        self.setFixedSize(540, 43)          
 #Text field
         self.address=QLineEdit()
         self.address.setStyleSheet("QLineEdit{color:#ffffff; background-color:#2a2a2a; border: 2px solid #353535; border-radius: 3px;font-size: 12px;}"
@@ -20,7 +20,7 @@ class Main(QMainWindow):
         self.address.setAlignment(Qt.AlignCenter)
         self.address.setFixedSize(440, 35)
         self.address.returnPressed.connect(self.navigatex)
-#About
+#About button
         self.about1 = QPushButton()
         self.about1.setStyleSheet("QPushButton{color:#ffffff; background-color:#353535; border: 2px solid #353535; border-radius: 3px;font-size: 12px;}"
         "QPushButton:hover{background-color:#5c5c5c;}")  
@@ -35,13 +35,21 @@ class Main(QMainWindow):
         self.addToolBar(self.toolbar)
 #About messagebox
     def about(self):
-        buttonReply = QMessageBox.information(self, 'Runner-QT v.3.0.1 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix", QMessageBox.Ok )
+        buttonReply = QMessageBox.information(self, 'Runner-QT v.4 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix _____________________________________________________________________________________________\n\nWhen Runner-QT exits(from v.4 onwards) it prints an error to the cli if a program it tried to execute did not exist. Runner-QT v-4 quits with an ESC key - automatic exit functionality remains as a feature.", QMessageBox.Ok )
         if buttonReply == QMessageBox.Ok:
-            pass
+            pass    
+#Open program    
     def navigatex(self):
-        location=self.address.text()
-        run=subprocess.Popen(location, shell=True)
-        self.close()                                    
+        try:
+            location=self.address.text()
+            run=subprocess.Popen(location, shell=True)
+            self.close()  
+        except Exception as e: 
+            print (e);
+#Quit program - Notice that this is the main window. We can pass the ESC event directly into it - since we only destroy/exit the Gui.
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.close()           
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Main()
