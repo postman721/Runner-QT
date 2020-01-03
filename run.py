7f2dbcfa190aa7a71962e8baebcc784bbd10e71e
JJ Posti <jjposti@techtimejourney.net>
2020-01-03 19:17:13 +0200

    Runner QT to version 6
    Subprocess.Popen is removed due to potential security issues and replaced with QProcess.

--- run.py
+++ run.py
@@ -1,24 +1,23 @@
-#Runner-QT v.4 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
+#Runner-QT v.6 Copyright (c) 2017 JJ Posti <techtimejourney.net> This program comes with ABSOLUTELY NO WARRANTY;  for details see: http://www.gnu.org/copyleft/gpl.html.  This is free software, and you are welcome to redistribute it under  GPL Version 2, June 1991")
 #!/usr/bin/env python
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import *  
-from PyQt5.QtWidgets import *
-import subprocess, os, sys
-from subprocess import Popen
+from PyQt5.QtCore import Qt  
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton, QToolBar, QMessageBox
+import os, sys
 class Main(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Main, self).__init__(*args, **kwargs)
 #Title & Layout	& Label	
-        self.setWindowTitle("Runner-QT")
-        self.setStyleSheet("color:#ffffff; background-color:#353535; border: 2px solid #353535; border-radius: 3px;font-size: 12px;")
+        self.setWindowTitle("Enter program")
+        self.setStyleSheet("color:#ffffff; background-color:#353535;font-size: 12px;")
         self.move(QApplication.desktop().screen().rect().center()- self.rect().center())
-        self.setFixedSize(540, 43)          
+        self.setFixedSize(400, 36)          
 #Text field
         self.address=QLineEdit()
         self.address.setStyleSheet("QLineEdit{color:#ffffff; background-color:#2a2a2a; border: 2px solid #353535; border-radius: 3px;font-size: 12px;}"
-        "QLineEdit:focus{background-color:#5c5c5c;}")          
+        "QLineEdit:focus{background-color:#353535;}")          
         self.address.setAlignment(Qt.AlignCenter)
-        self.address.setFixedSize(440, 35)
+        self.address.setFixedSize(340, 35)
         self.address.returnPressed.connect(self.navigatex)
 #About button
         self.about1 = QPushButton()
@@ -26,7 +25,7 @@ class Main(QMainWindow):
         "QPushButton:hover{background-color:#5c5c5c;}")  
         self.about1.setObjectName("About")
         self.about1.setText("About")
-        self.about1.setFixedSize(60, 35)
+        self.about1.setFixedSize(40, 35)
         self.about1.clicked.connect(self.about)                
 #Toolbar & Layout
         self.toolbar=QToolBar()
@@ -35,17 +34,19 @@ class Main(QMainWindow):
         self.addToolBar(self.toolbar)
 #About messagebox
     def about(self):
-        buttonReply = QMessageBox.information(self, 'Runner-QT v.4 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix _____________________________________________________________________________________________\n\nWhen Runner-QT exits(from v.4 onwards) it prints an error to the cli if a program it tried to execute did not exist. Runner-QT v-4 quits with an ESC key - automatic exit functionality remains as a feature.", QMessageBox.Ok )
+        buttonReply = QMessageBox.information(self, 'Runner-QT v.6 Copyright (c) 2017 JJ Posti <techtimejourney.net>', "Runner-QT  is a dialog for running programs. The program comes with ABSOLUTELY NO WARRANTY  for details see: http://www.gnu.org/copyleft/gpl.html. This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991.\n \nRunner-QT runs programs with or without arguments (Example: firefox www.google.fi or albix _____________________________________________________________________________________________\n\n", QMessageBox.Ok )
         if buttonReply == QMessageBox.Ok:
             pass    
 #Open program    
     def navigatex(self):
         try:
             location=self.address.text()
-            run=subprocess.Popen(location, shell=True)
-            self.close()  
+            self.process = QtCore.QProcess()
+            self.process.startDetached(str(location))
+            self.close();
         except Exception as e: 
             print (e);
+            self.close()
 #Quit program - Notice that this is the main window. We can pass the ESC event directly into it - since we only destroy/exit the Gui.
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
